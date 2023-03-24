e8ac3e1edb5094a0bdb8344b56cb6432d8f6ffb7
Hugo Lima Romão <hugo8romao@gmail.com>
2023-03-24 13:00:43 -0400

    fix jest error

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 const nextJest = require('next/jest')
 
-const createJestConfig = nextJest({
-  dir: './',
-})
+const createJestConfig = nextJest()
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', 'src']
 }
 
 module.exports = createJestConfig(customJestConfig)
